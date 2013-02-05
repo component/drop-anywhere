f34a1d9ba5cdf1bd6dd3214d5b998114db65fb15
TJ Holowaychuk <tj@vision-media.ca>
2013-02-05 14:10:16 -0800

    fix multiple support

--- index.js
+++ index.js
@@ -6,7 +6,18 @@
 var Dropload = require('dropload')
   , events = require('events');
 
+/**
+ * Expose `DropAnywhere`.
+ */
+
 module.exports = DropAnywhere;
+
+/**
+ * Make the document droppable and invoke `fn(err, upload)`.
+ *
+ * @param {Function} fn
+ * @api public
+ */
 
 function DropAnywhere(fn) {
   if (!(this instanceof DropAnywhere)) return new DropAnywhere(fn);
@@ -15,26 +26,38 @@ function DropAnywhere(fn) {
   this.el.id = 'drop-anywhere';
   this.events = events(this.el, this);
   this.docEvents = events(document.body, this);
-  this.events.bind('dragleave');
-  this.docEvents.bind('dragenter');
+  this.events.bind('drop', 'remove');
+  this.events.bind('dragleave', 'remove');
+  this.docEvents.bind('dragenter', 'add');
   this.drop = Dropload(this.el);
   this.drop.on('error', fn);
-  this.drop.on('upload', function(upload){
-    self.ondragleave();
-    fn(null, upload);
-  });
+  this.drop.on('upload', function(upload){ fn(null, upload) });
 }
 
-DropAnywhere.prototype.ondragenter = function(){
+/**
+ * Add the element.
+ */
+
+DropAnywhere.prototype.add = function(){
   document.body.appendChild(this.el);
 };
 
-DropAnywhere.prototype.ondragleave = function(){
+/**
+ * Remove the element.
+ */
+
+DropAnywhere.prototype.remove = function(){
   document.body.removeChild(this.el);
 };
 
+/**
+ * Unbind.
+ *
+ * @api public
+ */
+
 DropAnywhere.prototype.unbind = function(){
-  this.drop.unbind();
-  this.events.unbind();
   this.docEvents.unbind();
+  this.events.unbind();
+  this.drop.unbind();
 };
